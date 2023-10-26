ef99222ccb93b238c33a08d7f39037c130cd8334
KonstFed <konstantin_fedorov_03@mail.ru>
2023-10-26 22:03:35 +0300

    bug fix

--- src/preprocessing/build.py
+++ src/preprocessing/build.py
@@ -9,6 +9,6 @@ def _build(local_conifg):
 def build_preprocessing(preprocessing_config: dict):
     tokenizer = _build(preprocessing_config.tokenizer)
     text2vector: Text2Vector = _build(preprocessing_config.text2vector)
-    if preprocessing_config.text2vector.load_path is not {}:
+    if preprocessing_config.text2vector.get("load_path", None) is not None:
         text2vector = text2vector.load(preprocessing_config.text2vector.load_path)
     return tokenizer, text2vector
