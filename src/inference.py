21be598ec2a441cc9d97014563f6457d37713dca
KonstFed <konstantin_fedorov_03@mail.ru>
2023-11-03 23:28:31 +0300

    t5 inference

--- src/inference.py
+++ src/inference.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 from addict import Dict
 import yaml
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 from .models import build_model
 from .preprocessing import build_preprocessing
@@ -47,25 +48,60 @@ class ToxicClassificationPipeline:
         with torch.no_grad():
             input_vector = torch.tensor(input_vector, dtype=torch.float32)
             return self.model(input_vector).detach()
-        
+
 
 class BertPipeline:
     def __init__(self, config) -> None:
-        self.tokenizer: Tokenizer = build_preprocessing(config.preprocessing)["tokenizer"]
+        self.tokenizer: Tokenizer = build_preprocessing(config.preprocessing)[
+            "tokenizer"
+        ]
         self.model = build_model(config.model, config.training)
         self.model.eval()
 
     def forward(self, input: str) -> float:
         tokens = self.tokenizer.forward([input])
-        tokens = {k:torch.tensor(v) for k, v in tokens.items()}
 
         out = self.model(tokens)
         p = torch.nn.functional.sigmoid(out["logits"].detach())
         return p
 
+
+class ParaphrasingTransformerPipeline:
+    def __init__(self, config) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(config.model.model_name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(config.model.pretrained_path)
+        self.config = config
+        self._tokenizer_args = {
+            k: v
+            for k, v in config.preprocessing.tokenizer.items()
+            if k not in ["name", "model_name"]
+        }
+        self.model.eval()
+
+    def forward(self, input: str) -> str:
+        tokens = self.tokenizer(input, return_tensors="pt", **self._tokenizer_args)
+        # tokens = {k: torch.tensor(v) for k, v in tokens.items()}
+        out = self.model.generate(
+            input_ids=tokens["input_ids"],
+            attention_mask=tokens["attention_mask"],
+            max_length=50,
+            num_return_sequences=1,
+        )
+        preds = [
+            self.tokenizer.decode(
+                gen_id, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )
+            for gen_id in out
+        ]
+
+        return preds
+
+
 def inference(config, input: str):
-    if config.model.name == "DistilBert":
+    if config.model.name in ["DistilBert"]:
         pipeline = BertPipeline(config)
+    elif config.model.name == "T5model":
+        pipeline = ParaphrasingTransformerPipeline(config)
     else:
         pipeline = ToxicClassificationPipeline(config)
     return pipeline.forward(input)
