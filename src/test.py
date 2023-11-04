0d7704853e266cb447c2a220bba65c778e661d99
KonstFed <konstantin_fedorov_03@mail.ru>
2023-11-04 14:14:06 +0300

    batching in test

--- src/test.py
+++ src/test.py
@@ -3,8 +3,9 @@ import os
 
 from addict import Dict
 import yaml
-from tqdm import tqdm
+from tqdm import trange
 import csv
+import torch
 
 from .inference import ParaphrasingTransformerPipeline
 from .models import build_model
@@ -21,12 +22,15 @@ def get_args():
     parser.add_argument(
         "-o", "--output", help="path to folder where put logs", required=False, default="."
     )
+    parser.add_argument(
+        "-b", "--batch-size", help="batch size for input", default=32, type=int
+    )
     parser.add_argument("path", help="Path to file to process")
 
     args = parser.parse_args()
     with open(args.config, "r") as file:
         config = yaml.safe_load(file)
-    return Dict(config), args.path, args.output
+    return Dict(config), args.path, args.output, args.batch_size
 
 def _load_data(path) -> list[str]:
     texts = []
@@ -37,21 +41,20 @@ def _load_data(path) -> list[str]:
             texts.append(row[1])
     return texts
 
-def test_transformer(config, test_data: list[str], save_folder: str):
 
+def test_transformer(config, test_data: list[str], save_folder: str, batch_size):
     pipeline = ParaphrasingTransformerPipeline(config)
     _out_path = os.path.join(save_folder, "output.tsv")
     with open(_out_path, "w", newline='') as file:
         writer = csv.writer(file, delimiter="\t")
         writer.writerow(["reference", "translation"])
-    
-        for sentence in tqdm(test_data):
-            output = pipeline.forward(sentence)
-            writer.writerow([sentence, output[0]])
+        for idx in trange(0, len(test_data), batch_size):
+            output = pipeline.forward(test_data[idx: idx + batch_size])
+            writer.writerows([(test_data[idx + i], output[i]) for i in range(len(output))])
     build_model(config.model, config.training)
 
 
 if __name__ == "__main__":
-    config, path, folder = get_args()
+    config, path, folder, b_size = get_args()
     data = _load_data(path)
-    test_transformer(config, data, folder)
+    test_transformer(config, data, folder, b_size)
